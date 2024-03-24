9949425fc7ae5d499bda8a20863bbbf343c5699e
Kris <kristian_dilov@abv.bg>
2024-03-24 11:14:07 +0000

    sum function

--- codewars.c
+++ codewars.c
@@ -1,6 +1,25 @@
 
 #include <stdio.h>
 
+int compare(const void * a, const void * b){
+    return (*(int *)a - *(int *) b );
+}
+
+
+int sum(const int numbers[/*length*/], int length)
+{
+    qsort(numbers, length, sizeof(int), compare);
+    int sum = 0;
+
+    for(int i = 1; i<length-1; i++){
+        sum+= numbers[i];
+    }
+
+    return sum;
+
+} 
+
+
 int findMin(int numbers[], int length) {
 
     int min_value = numbers[0];
@@ -26,8 +45,11 @@ int findMax(int numbers[], int length) {
 }
 
 
+
+
+
 int main(){
-    int numbers[4] = {2,3,10, 1};
+    int numbers[5] = {2,3,10, 1, 1};
 
     int res = findMin(numbers, 4);
     int res2 = findMax(numbers, 4);
@@ -35,6 +57,18 @@ int main(){
     printf("Result: %d\n", res);
     printf("Result: %d\n", res2);
 
+    // int size = sizeof(numbers) / sizeof(numbers[0]);
+    // qsort(numbers, size, sizeof(int), compare);
+
+    int sum1 = sum(numbers, 5);
+    printf("Result: %d\n", sum1);
+
+
+
+   printf("\nAfter sorting the list is: \n");
+   for(int n = 0 ; n < 5; n++ ) {   
+      printf("%d ", numbers[n]);
+   }
 
     return 0;
 
