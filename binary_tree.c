1bfcb7f11fd74c70c408a1e743cf2e419ef9a900
Kris <kristian_dilov@abv.bg>
2024-05-06 21:51:58 +0100

    Add left nodes

--- binary_tree.c
+++ binary_tree.c
@@ -4,22 +4,52 @@
 typedef struct node {
 
   int val;
-  struct *node left;
-  struct *node right;
-
+  struct node *left;
+  struct node *right;
 
 } node_t;
 
+void insert_node(node_t *tree, int value) {
+  if (tree->val == 0) {
+    tree->val = value;
+
+  } else {
+    __insert_node_recursive(*tree, value);
+  }
+}
+
+void __insert_node_recursive(node_t *tree, int value) {
+
+  if (tree->val > value) {
+ 
+    if (tree->left != NULL) {
+       __insert_node_recursive(tree->left, value);
+
+         } else {
 
 
-int main(){
+      node_t *sub_tree = (node_t *)malloc(sizeof(node_t));
+      sub_tree->left = NULL;
+      sub_tree->right = NULL;
+      sub_tree->val = value;
+      tree->left = sub_tree;
+
+         }
+  }
+
+  elif (tree->val < value) {
+    if (tree->right->val == 0) {
+      tree->right->val = value;
+    } else {
+      __insert_node_recursive(tree->right, value);
+    }
+  }
+}
+
+int main() {
 
   node_t *test_list = (node_t *)malloc(sizeof(node_t));
   test_list.val = 0;
   test_list.left = NULL;
   test_list.right = NULL;
-
-
-
-
 }
