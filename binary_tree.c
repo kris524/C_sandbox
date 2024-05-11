e204020e040df387d486f7b88513d71c9050a9ec
Kris <kristian_dilov@abv.bg>
2024-05-11 16:59:04 +0100

    Add tree print

--- binary_tree.c
+++ binary_tree.c
@@ -9,7 +9,6 @@ typedef struct node {
 
 } node_t;
 
-
 void insert_node_recursive(node_t *tree, int value) {
 
   if (tree->val > value) {
@@ -28,23 +27,18 @@ void insert_node_recursive(node_t *tree, int value) {
   }
 
   else if (tree->val <= value) {
-      
-    if (tree->right != NULL){
+
+    if (tree->right != NULL) {
       insert_node_recursive(tree->right, value);
-    }
-    else {
+    } else {
       node_t *sub_tree = (node_t *)malloc(sizeof(node_t));
       sub_tree->left = NULL;
       sub_tree->right = NULL;
       sub_tree->val = value;
       tree->right = sub_tree;
-
     }
-
   }
 }
-
-
 
 void insert_node(node_t *tree, int value) {
   if (tree->val == 0) {
@@ -55,12 +49,20 @@ void insert_node(node_t *tree, int value) {
   }
 }
 
+void print_tree(node_t *tree) {
 
-void show_tree(node_t* tree){
+  if (tree == NULL) {
+    return;
+  }
+  if (tree->left != NULL) {
+    print_tree(tree->left);
+  }
+  printf("%d ", tree->val);
 
+  if (tree->right != NULL) {
+    print_tree(tree->right);
+  }
 }
-
-
 
 int main() {
 
@@ -69,13 +71,12 @@ int main() {
   test_list->left = NULL;
   test_list->right = NULL;
 
-  insert_node(test_list,5);
-  insert_node(test_list,8);
-  insert_node(test_list,4);
-  insert_node(test_list,3);
+  insert_node(test_list, 5);
+  insert_node(test_list, 8);
+  insert_node(test_list, 4);
+  insert_node(test_list, 3);
+
+  print_tree(test_list);
 
   free(test_list);
-
-
-
 }
