e8d05bca122d3903f3f78531d3fc3dab8ea23e0b
Kris <kristian_dilov@abv.bg>
2024-05-27 14:23:31 +0100

    fix anagram

--- anagram.c
+++ anagram.c
@@ -4,10 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int *create_array_map(char *str) {
 
-int* create_array_map(char* str) {
-
-  int* ascii_array = malloc(256 * sizeof(int));  
+  int *ascii_array = malloc(256 * sizeof(int));
 
   for (int t = 0; str[t] != '\0'; t++) {
 
@@ -17,10 +16,29 @@ int* create_array_map(char* str) {
   return ascii_array;
 }
 
-bool anagram(char first_string, char second_string) {
+bool anagram(char* first_string, char* second_string) {
+
+  int *first_arr = create_array_map(first_string);
+  int *second_arr = create_array_map(second_string);
+
+  for (int i = 0; i < 256; i++) {
+    if (first_arr[i] != second_arr[i]) {
+      return false;
+    }
+  }
+  free(first_arr);
+  free(second_arr);
+  return true;
+}
+
+int main() { 
+
+  bool res = anagram("ABC", "BCAD"); // returns 0
+  printf("%d\n", res);
 
 
-  int* first_arr = create_array_map(first_string); 
-  int* second_arr = create_array_map(second_string); 
+  bool res2 = anagram("ABC", "BCA"); // returns 1
+  printf("%d\n", res2);
+
 
 }
